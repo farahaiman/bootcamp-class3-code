1c0a3f8d939d1c6635a146e3d1fb0385ab8f23db
farah <farahaiman548@gmail.com>
2020-11-11 21:38:20 +0500

    child to perent passing data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Briyani from './briyani';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+     <h1>Asslam Alaikum Sir Plz Send Your review</h1>
+     <Briyani sirReviwe="best of luck" sirBestIdea="Read more keep practicing" />
+     <hr/>
+     <Briyani sirReviwe="best wishes for u" sirBestIdea="try different things " />
+     < hr/>
+     <Briyani sirReviwe="do some creative" sirBestIdea="Allah Hafiz" />
+     
+       
     </div>
   );
 }
---
+++ src/briyani.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Briyani(props) {
+    return (
+        <div>
+            <h2> Great but sakht mehnat ki zarort h {props.sirReviwe}</h2>
+            <h2> YOU Will do it {props.sirBestIdea}</h2>
+            
+        </div>
+    )
+}
+
+export default Briyani
