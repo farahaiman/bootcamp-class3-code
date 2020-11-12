b349e561083294c1518ea7f2d2655300fc1c06f8
farah <farahaiman548@gmail.com>
2020-11-12 09:01:32 +0500

    comment

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ function App() {
       
      <h1>Asslam Alaikum Sir Plz Send Your review</h1>
      <Briyani sirReviwe="best of luck" sirBestIdea="Read more keep practicing" />
+     < hr/>
+     <Briyani sirReviwe="c u next time" sirBestIdea="ok" />
+     
      <hr/>
      <Briyani sirReviwe="best wishes for u" sirBestIdea="try different things " />
      < hr/>
